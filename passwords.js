bec2f49ecaaa1a6700c6abd52f3215ae8a1a8a59
Michael Bechtel <m783b224@ku.edu>
2016-02-25 20:58:47 -0600

    Fix logic for Password Validator

--- passwords.js
+++ passwords.js
@@ -3,9 +3,9 @@ function passwordCheck()
     var password = document.getElementById("pw1").value;
     var password2 = document.getElementById("pw2").value;
     
-    if(password.length < 8)
+    if(password.length < 8 || password2.length < 8)
     {
-        alert("The password does not meet the minimum requirement of 8 characters");
+        alert("One of the passwords does not meet the minimum requirement of 8 characters");
     }
     else if(password == password2)
     {
